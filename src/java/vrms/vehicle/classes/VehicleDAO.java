8bc701ce1c7caf59abf5b0bc4c72ae3040adf3b9
SudasunViraj <mvirajsudasun@gmail.com>
2020-09-19 13:50:20 +0530

    Add Vehicle Servlet OK

--- src/java/vrms/vehicle/classes/VehicleDAO.java
+++ src/java/vrms/vehicle/classes/VehicleDAO.java
@@ -5,10 +5,62 @@
  */
 package vrms.vehicle.classes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author HP
  */
 public class VehicleDAO {
+
+    public int insert(VehicleBean vehicle) {
+        int result = 0;
+        try {
+            String url = "jdbc:mysql://localhost:3306/vrms_db";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, "root", "");
+            PreparedStatement ps = con.prepareStatement("INSERT INTO vehicle VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+            ps.setString(1, vehicle.getMake());
+            ps.setString(2, vehicle.getModel());
+            ps.setString(3, vehicle.getYor());
+            ps.setString(4, vehicle.getVin());
+            ps.setString(5, vehicle.getServ_from());
+            ps.setString(6, vehicle.getCategory());
+            ps.setString(7, vehicle.getRate_per_day_wod());
+            ps.setString(8, vehicle.getRate_per_week_wod());
+            ps.setString(9, vehicle.getRate_per_month_wod());
+            ps.setString(10, vehicle.getExcess_milage_wod());
+            ps.setString(11, vehicle.getRate_per_day_wd());
+            ps.setString(12, vehicle.getExcess_milage_wd());
+            result = ps.executeUpdate();
+            ps.close();
+            con.close();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(VehicleDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return result;
+        
+    }
+    
+    public ResultSet view(){
+        ResultSet result = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/vrms_db";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, "root", "");
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM vehicle");
+            result = ps.executeQuery();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(VehicleDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
     
 }
--- web/web_content/vehicles/view_vehicle.jsp
+++ web/web_content/vehicles/view_vehicle.jsp
@@ -176,7 +176,7 @@
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 medium">Kavinda Jayasekara</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 medium"><%= session.getAttribute("first_name")%></span>
                 <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
               </a>
             </li>
